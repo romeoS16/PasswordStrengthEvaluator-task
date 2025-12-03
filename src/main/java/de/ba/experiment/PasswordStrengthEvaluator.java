0ced6b45fa85d3b8fa3642861aec0a99c00699a5
romeoS16 <romeoschweizer29@gmail.com>
2025-12-03 10:37:46 +0100

    ergänzung

--- src/main/java/de/ba/experiment/PasswordStrengthEvaluator.java
+++ src/main/java/de/ba/experiment/PasswordStrengthEvaluator.java
@@ -1,4 +1,16 @@
 package de.ba.experiment;
 
 public class PasswordStrengthEvaluator {
+    /**
+     * Bewertet die Stärke eines Passworts.
+     *
+     * (Siehe ausführliche Beschreibung im Aufgabenblatt)
+     *
+     * @param password das zu prüfende Passwort
+     * @return "WEAK", "MEDIUM" oder "STRONG" nach Definition
+     */
+    public static String evaluateStrength(String password) {
+        // TODO: implementieren
+        return "WEAK";
+    }
 }
--- src/test/java/PasswordStrengthEvaluatorTest.java
+++ src/test/java/PasswordStrengthEvaluatorTest.java
@@ -1,2 +1,16 @@
+import de.ba.experiment.PasswordStrengthEvaluator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PasswordStrengthEvaluatorTest {
+
+    @Test
+    public void nullPassword_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength(null);
+        assertEquals("WEAK", result);
+    }
+
+    // TODO: weitere Tests ergänzen
+
 }
